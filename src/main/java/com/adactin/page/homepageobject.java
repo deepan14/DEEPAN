789b10f08092fba3f048249df16aa0e1c566cb30
deepan <deepanking3@gmail.com>
2020-01-08 10:14:03 +0530

    hi

--- src/main/java/com/adactin/page/homepageobject.java
+++ src/main/java/com/adactin/page/homepageobject.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 public class homepageobject extends Basepage{
 	@FindBy(id="username")
 
-	private WebElement adactinid23;
+	private WebElement adactinid23123;
 
 	private WebElement adactinid;
 
